5b3132aed23b53e8ce4477cea122761da0c85d17
Harshit-Navik <hrxit1984@gmail.com>
2026-08-24 21:57:19 +0530

    update user routes

---
+++ server/src/routes/user.route.ts
@@ -0,0 +1,37 @@
+import { Router } from "express";
+import userController from "../controllers/user.controller";
+import authMiddleware from "../middlewares/auth.middleware";
+
+const router = Router();
+
+router
+    .route("/signup")
+    .post(userController.signUp);
+
+router
+    .route("/signin")
+    .post(userController.signIn);
+
+
+// secured routes
+
+router.use(authMiddleware);
+
+router
+    .route("/")
+    .get(userController.me);
+
+router
+    .route("/logout")
+    .post(userController.logOut);
+
+router
+    .route("/token")
+    .post(userController.refreshToken)
+
+router
+    .route("/password")
+    .post(userController.changePassword);
+
+
+export default router;
