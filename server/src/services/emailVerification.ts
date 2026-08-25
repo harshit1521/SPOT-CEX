e21831e59b1e6ba639ca5a78358497eaee131910
Harshit-Navik <hrxit1984@gmail.com>
2026-08-25 23:38:03 +0530

    add email verifation service

--- server/src/services/emailVerification.ts
+++ server/src/services/emailVerification.ts
@@ -4,4 +4,11 @@ const generateVerificationToken = (): string => {
   return crypto.randomBytes(32).toString("hex");
 };
 
-export { generateVerificationToken };
+const hasVerificationToken = (token: string): string => {
+    return crypto
+    .createHash("sha256")
+    .update(token)
+    .digest("hex");
+}
+
+export { generateVerificationToken, hasVerificationToken};
