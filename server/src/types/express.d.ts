26fa604859877a9698ad9950004b3d982fd6e538
Harshit-Navik <hrxit1984@gmail.com>
2026-08-27 08:42:49 +0530

    add express types

---
+++ server/src/types/express.d.ts
@@ -0,0 +1,13 @@
+declare global {
+    namespace Express {
+        interface Request {
+            user? : {
+                id: number,
+                username: string,
+                email: string
+            }
+        }
+    }
+}
+
+export {}
