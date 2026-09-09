332e7687a5389214a995e91c526a431e540a0495
Harshit-Navik <hrxit1984@gmail.com>
2026-09-09 17:05:32 +0530

    update app configuration: fix route paths, add exchange routes, and initialize engine service

--- server/app.ts
+++ server/app.ts
@@ -1,13 +1,14 @@
 import express from "express";
 import cors from "cors";
-import cookieParser from "cookie-parser"
+import cookieParser from "cookie-parser";
 import { errorHandler } from "./src/middlewares/errorHandler";
+import { initializeEngine } from "./src/services/engine.service.ts";
 
 const app = express();
 
 // ---------------------------- middlewares ----------------------------
 
-app.use( cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
+app.use(cors({ origin: process.env.CORS_ORIGIN, credentials: true }));
 app.use(express.json({ limit: "16kb" }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
@@ -15,13 +16,18 @@ app.use(cookieParser());
 
 // ---------------------------- import routes ----------------------------
 
-import user from "./src/routes/user.route.ts"
+import user from "./src/routes/user.route.ts";
+import exchange from "./src/routes/exchange.route.ts";
 
 // ---------------------------- route declaration ----------------------------
 
-app.use("api/v1/users", user);
+app.use("/api/v1/users", user);
+app.use("/api/v1/exchange", exchange);
 
 // ---------------------------- error handler ----------------------------
 
 app.use(errorHandler);
-app.listen(3000, () => { console.log(`server is listening to port: 3000`) })
+
+await initializeEngine();
+
+app.listen(3000, () => { console.log("server is listening to port: 3001"); });
